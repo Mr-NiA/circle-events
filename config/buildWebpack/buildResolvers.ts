e5ccfad6a66b1ff7b5f59c21dda5ef24ba8b3357
Ivan Nogin <nogin.vanya2015@mail.ru>
2025-09-05 19:35:43 +0300

    webpack-var

---
+++ config/buildWebpack/buildResolvers.ts
@@ -0,0 +1,15 @@
+import path from "path";
+import type { ResolveOptions } from "webpack";
+import type { BuildOptions } from "./types/config";
+
+export function buildResolvers(options: BuildOptions): ResolveOptions {
+  return {
+    extensions: [".tsx", ".ts", ".js"],
+    preferAbsolute: true,
+    modules: [options.paths.src, "node_modules"],
+    mainFiles: ["index"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  };
+}
---
+++ config/buildWebpack/buildWebpackConfig.ts
@@ -0,0 +1,36 @@
+import type webpack from "webpack";
+
+import { buildDevServer } from "./buildDevServer";
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+import type { BuildOptions } from "./types/config";
+
+export function buildWebpackConfig(
+  options: BuildOptions,
+): webpack.Configuration {
+  const { paths, mode, isDev } = options;
+
+  console.log("paths", paths);
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(options),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+  };
+}
